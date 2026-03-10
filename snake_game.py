e498b78efcbf689b27364c0bc0e89f9d87eea6e3
Zion Umoh <y6s68@students.keele.ac.uk>
2026-03-10 13:36:35 +0000

    Feat: Init snake and food objects on canvas

--- snake_game.py
+++ snake_game.py
@@ -11,10 +11,27 @@ class SnakeGame:
     def __init__(self):
         self.window = tk.Tk()
         self.window.title("Keele Snake Game")
+        
+        self.score = 0
+        self.direction = 'down'
+        
         self.canvas = tk.Canvas(self.window, bg="black", width=WIDTH, height=HEIGHT)
         self.canvas.pack()
-        self.window.update()
+        
+        # Initial Snake positions
+        self.snake_coords = [[0, 0], [0, 20], [0, 40]]
+        self.squares = []
+        
+        for x, y in self.snake_coords:
+            square = self.canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill="green")
+            self.squares.append(square)
 
+        self.food_coords = [random.randint(0, (WIDTH//SPACE_SIZE)-1) * SPACE_SIZE, 
+                            random.randint(0, (HEIGHT//SPACE_SIZE)-1) * SPACE_SIZE]
+        self.food = self.canvas.create_oval(self.food_coords[0], self.food_coords[1], 
+                                            self.food_coords[0] + SPACE_SIZE, 
+                                            self.food_coords[1] + SPACE_SIZE, fill="red")
+    
 if __name__ == "__main__":
     game = SnakeGame()
     game.window.mainloop()
